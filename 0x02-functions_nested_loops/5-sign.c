97d65098afe200c281d5b1fa618bafe3b1f344f0
YassineNabat <nabatyassine1@gmail.com>
2023-09-12 19:25:21 +0100

    Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *  * print_sign - prints the sign of a number
- *   * @n: the int to check
- *    * Return: 1 and prints + if n is greater than zero
- *     * 0 and prints 0 if n is zero
- *      * -1 and prints - if n is less than zero
- *       */
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
 int print_sign(int n)
 {
 	if (n > 0)
@@ -16,7 +16,7 @@ int print_sign(int n)
 	} else if (n == 0)
 	{
 		_putchar('0');
-		return(0):
+		return(0);
 	}else
 	{
 		_putchar('-');
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < a0; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for(j = 0;j < 10; j++)
 		{
