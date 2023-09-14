5f713021d406c075d3008e5e8785dd1b42a06999
YassineNabat <nabatyassine1@gmail.com>
2023-09-14 17:25:03 +0100

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  * more_numbers - print more numbers
+ *   */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	while (i < 10)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar('j');
+		}
+		i++;
+		_putchar('\n');
+	}
+}
