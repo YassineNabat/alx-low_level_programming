e7113c6a89f07b7a1fc929f464fb345e1c4f2382
YassineNabat <nabatyassine1@gmail.com>
2023-12-07 15:53:12 +0100

    add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,9 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newNode ->prev = NULL;
 
 	if (*head != NULL)
-		*head->prev = new;
-
-	*head = newNode;
+		*head = newNode;
 
 	return newNode;    
 }
