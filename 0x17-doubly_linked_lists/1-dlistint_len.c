5fd5e26172d270bf405ec9a9ea088ff65148c8cf
YassineNabat <nabatyassine1@gmail.com>
2023-12-07 15:48:59 +0100

    add node

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
-/*
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: pointer to the head of the list
  *
- *
- *
- * 
+ * Return: number of nodes
  */
 
 size_t dlistint_len(const dlistint_t *h){
@@ -12,12 +12,12 @@ size_t dlistint_len(const dlistint_t *h){
 	int nbr = 0;
 	
 	if (h == NULL)
-		return nbr;
+		return (nbr);
 		    
 	while (h != NULL)
 	{
 		h = h -> next;
 		nbr++;
 	}
-	return nbr;
+	return (nbr);
 }
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * @head: double pointer to the head of the list
+ * @n: data to be added
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newNode;
+
+	newNode = malloc(sizeof(dlistint_t));
+	newNode->n = n;
+	newNode->next = (*head);
+	newNode ->prev = NULL;
+
+	if (*head != NULL)
+		*head = newNode;
+
+	return newNode;    
+}
