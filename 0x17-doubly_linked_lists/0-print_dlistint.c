ea5f6488d8938f2c4cf636dd04dbf7551576f158
YassineNabat <nabatyassine1@gmail.com>
2023-12-07 14:48:57 +0100

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,7 +7,7 @@
  * Return: The number of nodes in the list.
  */
 
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *tmp = h;
 	while (tmp) {
