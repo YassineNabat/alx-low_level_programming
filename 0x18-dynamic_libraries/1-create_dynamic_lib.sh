88537ae1bf81f2609ad87a5a1c1d9931c15e29e5
YassineNabat <nabatyassine1@gmail.com>
2023-12-11 17:31:16 +0100

    Create dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-gcc -Wall -Wextra -Werror -pendantic -c -fPIC *.c
+gcc -wall -wextra -werror -pendantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
