9d3133778bcb3b6c89a7bbaed37e66558ac833d2
YassineNabat <nabatyassine1@gmail.com>
2024-04-13 14:47:08 +0000

    del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -3,19 +3,19 @@
 
 /**
  * hash_table_delete - free a hash table.
- * @pht: A pointer to a hash table.
+ * @ht: A pointer to a hash table.
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *head = pht;
+	hash_table_t *head = ht;
 	hash_node_t *node, *tmp;
 	unsigned long int i;
 
-	for (i = 0; i < pht->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
-		if (pht->array[i] != NULL)
+		if (ht->array[i] != NULL)
 		{
-			node = pht->array[i];
+			node = ht->array[i];
 			while (node != NULL)
 			{
 				tmp = node->next;
