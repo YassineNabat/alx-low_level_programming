8b7550101aeadfea8acdcf54c09b8b64985a40c3
YassineNabat <nabatyassine1@gmail.com>
2024-04-17 14:53:58 +0100

    ht

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,18 +10,18 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int i;
-	hash_node_t *tp;
+	hash_node_t *tmp;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
 	    key == NULL || strlen(key) == 0)
 		return (NULL);
 	i = key_index((const unsigned char *)key, ht->size);
-	tp = ht->array[index];
-	while (!tp)
+	tmp = ht->array[i];
+	while (tmp== null)
 	{
-		if (strcmp(tp->key, key) == 0)
-		return (tp->value);
-		tp = tp->next;
+		if (strcmp(tmp->key, key) == 0)
+		return (tmp->value);
+		tmp = tmp->next;
 	}
 	return (NULL);
 }
